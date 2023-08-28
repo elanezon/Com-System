63bde3fd3d62efdbae4a4d47d0ff446f646b6d16
elanezon <elanezon@gamil.com>
2023-08-27 22:37:51 -0600

    added commentary

--- chanel_sim.py
+++ chanel_sim.py
@@ -3,26 +3,31 @@ import matplotlib.pyplot as plot
 import numpy as np
 
 #def GraphSignal()
+##Defines Plot size
 plot.rcParams["figure.figsize"] = [7.50, 3.50]
 plot.rcParams["figure.autolayout"] = True
-input_wave = read("tono.wav")
+
+input_wave = read("tono.wav")##Opens .wav file and extracts data
 signal = input_wave[1]
+
+##Plots signal
 plot.plot(signal[0:1024])
 plot.ylabel("Amplitude")
 plot.xlabel("Time")
 plot.show()
 
 #def GraphPSD()
-plot.psd(signal)
+plot.psd(signal)##Plots Signal PSD
 
 
 #def ChannelSim
-noise=np.random.normal(0, 0.1, signal.shape[0])
+noise=np.random.normal(0, 0.1, signal.shape[0]) ##Creates Gaussian Noise with STD = 0,1
 
-signal_noise = signal+noise
+signal_noise = signal+noise ##adds noise to original signal
 
-plot.psd(noise)
+plot.psd(signal_noise) ##Plots Channel PSD
 
+#Plots Channel Amplitude
 plot.plot(noise[0:1024])
 plot.ylabel("Amplitude")
 plot.xlabel("Time")
