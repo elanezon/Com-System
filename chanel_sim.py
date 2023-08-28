05fa73011ceedef55998cbc43f64388bb87c4813
elanezon <elanezon@gamil.com>
2023-08-27 22:17:39 -0600

    added channelsim function and PSDGraph function code

--- chanel_sim.py
+++ chanel_sim.py
@@ -7,21 +7,23 @@ plot.rcParams["figure.figsize"] = [7.50, 3.50]
 plot.rcParams["figure.autolayout"] = True
 input_wave = read("tono.wav")
 signal = input_wave[1]
-#plot.plot(signal[0:1024])
-#plot.ylabel("Amplitude")
-#plot.xlabel("Time")
-#plot.show()
+plot.plot(signal[0:1024])
+plot.ylabel("Amplitude")
+plot.xlabel("Time")
+plot.show()
 
 #def GraphPSD()
-#plot.psd(signal)
+plot.psd(signal)
 
+
+#def ChannelSim
 noise=np.random.normal(0, 0.1, signal.shape[0])
 
 signal_noise = signal+noise
 
 plot.psd(noise)
 
-#plot.plot(noise[0:1024])
-#plot.ylabel("Amplitude")
-#plot.xlabel("Time")
-#plot.show()
+plot.plot(noise[0:1024])
+plot.ylabel("Amplitude")
+plot.xlabel("Time")
+plot.show()
