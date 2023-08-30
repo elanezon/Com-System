09a699c42c038533c36f3c3fea63f1cc0c84905b
elanezon <elanezon@gmail.com>
2023-08-29 22:46:02 -0600

    Added clear after each plot to avoid graph mixing

--- chanel_sim.py
+++ chanel_sim.py
@@ -15,20 +15,29 @@ plot.plot(signal[0:1024])
 plot.ylabel("Amplitude")
 plot.xlabel("Time")
 plot.show()
+plot.clf()
 
 #def GraphPSD()
 plot.psd(signal)##Plots Signal PSD
-
+plot.show()
+plot.clf()
 
 #def ChannelSim
 noise=np.random.normal(0, 0.1, signal.shape[0]) ##Creates Gaussian Noise with STD = 0,1
 
 signal_noise = signal+noise ##adds noise to original signal
 
+plot.psd(noise) ##Plots noise PSD
+plot.show()
+plot.clf()
+
 plot.psd(signal_noise) ##Plots Channel PSD
+plot.show()
+plot.clf()
 
 #Plots Channel Amplitude
-plot.plot(noise[0:1024])
+plot.plot(signal_noise[0:1024])
 plot.ylabel("Amplitude")
 plot.xlabel("Time")
 plot.show()
+plot.clf()
